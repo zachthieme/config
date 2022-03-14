0d5cdadd53be5bf8872a5eaafe3e980c2cf9ec61
Zach Thieme <zach@techsage.org>
2022-03-13 21:21:22 -0700

    removed xmonad libs not in my version

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,8 +2,6 @@ import XMonad
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.StatusBar
-import XMonad.Hooks.StatusBar.PP
 
 main :: IO ()
 main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ myConfig
