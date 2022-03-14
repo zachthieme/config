9b3f316638198a8328d5f7d52f511c67154742e6
Zach Thieme <zach@techsage.org>
2022-03-13 21:18:30 -0700

    added xmobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,7 +1,9 @@
 import XMonad
-
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.StatusBar
+import XMonad.Hooks.StatusBar.PP
 
 main :: IO ()
-main = xmonad def
+main = xmonad $ ewmhFullscreen $ ewmh $ xmobarProp $ myConfig
