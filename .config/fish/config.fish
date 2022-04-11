a76a84c608981e6a7b54afa907fbd8bc7ed498ec
Zach Thieme <zach@techsage.org>
2022-04-10 23:18:39 -0700

    added alias to auto add files found by nf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,11 +27,11 @@ starship init fish | source
 
 # ALIASES
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-alias vi='hx'
 alias exa='exa --group-directories-first'
 alias l='exa'
 alias la='exa -la'
 alias a='exa -a'
 
 alias nf='fdfind . /home/zach/.config -H --changed-within 1d -E chromium -x /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME ls-files --others {} | uniq'
+alias nfa='fdfind . /home/zach/.config -H --changed-within 1d -E chromium -x /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME ls-files --others {} | uniq | rargs /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME add {0}'
 
