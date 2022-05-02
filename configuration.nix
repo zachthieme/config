25b70ce9b5addaa475763d3a232326e4483785a0
zachthieme <zach@techsage.org>
2022-05-02 15:44:02 -0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -9,6 +5,9 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
+
+  # make sure the config is stored in case you accidentally over write
+  system.copySystemConfiguration = true;
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
@@ -20,13 +19,24 @@
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
+  
   networking.networkmanager.enable = true;
-  networking.hostName = "mobile"; # Define your hostname.
+  networking = {
+    hostName = "mobile"; # Define your hostname.
 
-  # Set Networking Configs
-  networking.useDHCP = false;
-  networking.interfaces.enp0s21f0u5.useDHCP = true;
-  networking.interfaces.wlp2s0.useDHCP = true;
+    # Set Networking Configs
+    useDHCP = false;
+    interfaces = {
+      enp0s21f0u5.useDHCP = true;
+      wlp2s0.useDHCP = true;
+    };
+
+    # Open ports in the firewall.
+    firewall = {
+      allowedTCPPorts = [ 3389 ];
+      allowedUDPPortRanges = [ { from = 60000; to = 61000; } ];
+    };
+  };
 
   # Set locale
   i18n.defaultLocale = "en_US.UTF-8";
@@ -40,28 +50,33 @@
   users.defaultUserShell = pkgs.fish;
 
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.windowManager.i3.enable = true;
-  services.xserver.windowManager.xmonad.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      displayManager.lightdm.enable = true;
+      windowManager.i3.enable = true;
+      windowManager.xmonad.enable = true;
+    };
 
-  # Enable rdp
-  services.xrdp.enable = true;
-  services.xrdp.defaultWindowManager = "xmonad";
+    # needed for store VSCode auth token
+    gnome.gnome-keyring.enable = true;
 
-  # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
+    # Enable rdp
+    xrdp = {
+      enable = true;
+      defaultWindowManager = "xmonad";
+    };
 
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    # Enable the OpenSSH daemon.
+    openssh.enable = true;
+  };
 
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio.enable = true;
-
-  # needed for store VSCode auth token
-  services.gnome.gnome-keyring.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zach = {
@@ -72,56 +87,28 @@
   # add wheel users to sudoers
   security.sudo.wheelNeedsPassword = false;
 
-
   # install packages
   environment.systemPackages = with pkgs; [
-    neovim
     wget
     tmux
-    networkmanager
+    (
+      with import <nixpkgs> {};
+
+      vim_configurable.customize {
+        name = "vim";
+        vimrcConfig.customRC = ''
+          inoremap jj <Esc>
+          nnoremap JJJJ <Nop> 
+          set number relativenumber
+          set nowrap
+        '';
+        vimrcConfig.plug.plugins = with pkgs.vimPlugins; [vim-nix];
+      }
+     )
   ];
 
   nixpkgs.config.allowUnfree = true;
 
-  # Set newvim as default editor
-  environment.variables.EDITOR = "nvim";
-
-  # add aliases for neovim
-  nixpkgs.overlays = [
-    (self: super: {
-      neovim = super.neovim.override {
-        viAlias = true;
-        vimAlias = true;
-      };
-     })
- ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 3389 ];
-  networking.firewall.allowedUDPPortRanges = [ { from = 60000; to = 61000; } ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "21.11"; # Did you read the comment?
 
 }
