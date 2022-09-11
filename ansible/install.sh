ffe5986ead76d18e40ace1dca8dd599ca666f333
zachthieme <zach@techsage.org>
2022-09-11 15:14:38 -0700

    renamed and added an apt update

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,5 +1,6 @@
 adduser zach sudo
 apt install software-properties-common
 apt-add-repository ppa:ansible/ansible
+apt update
 apt install ansible
 ansible-galaxy collection install community.general
