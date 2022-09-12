67c212cd5d44a001e4177a51d29680866d4a9c0e
zachthieme <zach@techsage.org>
2022-09-12 10:47:34 -0700

    added switches to make headless

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,7 +1,10 @@
 adduser zach sudo
 sed -i "s/\%sudo    ALL=(ALL:ALL) ALL/\%sudo ALL=(ALL:ALL) NOPASSWD:ALL/" /etc/sudoers
-apt install software-properties-common
-apt-add-repository ppa:ansible/ansible
-apt update
-apt install ansible
+apt -y install software-properties-common
+apt-add-repository ppa:ansible/ansible -y
+apt -y update
+apt -y install ansible
 ansible-galaxy collection install community.general
+mkdir /home/zach/Code
+cd /home/zach/Code
+git clone https://github.com/zachthieme/config.git
