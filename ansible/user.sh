9527f327f862cb68f29b91f36e27d81d43ac4207
zachthieme <zach@techsage.org>
2022-09-12 21:09:13 -0700

    remove home-manager stuff

--- ansible/user.sh
+++ ansible/user.sh
@@ -2,18 +2,5 @@ mkdir /home/zach/Code
 cd /home/zach/Code
 git clone https://github.com/zachthieme/config.git
 
-# install nixos
-sh <(curl -L https://nixos.org/nix/install) --daemon
-#
-# install home manager 
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-nix-channel --add https://nixos.org/channels/nixos-unstable nixos-unstable
-nix-channel --update
-export NIX_PATH=$HOME/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels${NIX_PATH:+:$NIX_PATH}
-nix-shell '<home-manager>' -A install
-
 # link config files to proper place
 ln -s /home/zach/Code/config/dotfiles/* /home/zach/.config/
-
-# bring configuration live
-home-manager switch
