bed841d96ad0e5f5956e0fe7581c5663b84aa7fd
Zach Thieme <zach@techsage.org>
2022-09-15 19:50:15 -0700

    refacto for nvim

--- ansible/install.sh
+++ ansible/install.sh
@@ -2,8 +2,6 @@ adduser zach sudo
 sed -i "s/\%sudo\s*ALL=(ALL:ALL) ALL/\%sudo ALL=(ALL:ALL) NOPASSWD:ALL/" /etc/sudoers
 apt -y install software-properties-common
 apt-add-repository ppa:ansible/ansible -y
-apt-add-repository ppa:fish-shell/release-3 -y
-sudo add-apt-repository ppa:neovim-ppa/stable -y
 apt -y update
 apt -y install ansible
 ansible-galaxy collection install community.general
